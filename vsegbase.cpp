bcc57e34cd108ee5275f8fe4f9238cb28a7cf802
alvaro254 <i62agala@uco.es>
2019-03-22 19:14:34 +0100

    First commit

--- vsegbase.cpp
+++ vsegbase.cpp
@@ -46,7 +46,7 @@ int main (int argc, char * const argv[])
 	cmd.add(thres);
 	TCLAP::ValueArg<int> size("s", "radius", "Radiues value", false, 0, "int");
 	cmd.add(size);
-	  
+
 	// Parse input arguments
 	cmd.parse(argc, argv);
 
@@ -71,16 +71,16 @@ int main (int argc, char * const argv[])
 
 	//Open the input stream
 	VideoCapture input;
-	    
+
 	if (cameraInput)
 		input.open(atoi(filein));
-	else  
+	else
 		input.open(filein);
-	  	  
+
 	if (!input.isOpened())
 	{
 		cerr << "Error: the input stream is not opened.\n";
-	    return 0;
+		return 0;
 	}
 
 	Mat inFrame, inFrame2, Segmented_mask, Segmented_mask3C, foreground, splited[3], result, kernel, opening, closing;
@@ -94,18 +94,18 @@ int main (int argc, char * const argv[])
 		cerr << "Error: could not read any image from stream.\n";
 		return 0;
 	}
-	
+
 	double fps = 25.0;
 	if (!cameraInput)
 		fps=input.get(CV_CAP_PROP_FPS);
-	
+
 	//Open the output stream
 	VideoWriter output;
 	output.open(fileout, input.get(CV_CAP_PROP_FOURCC), fps, inFrame.size(), true);
 	if (!output.isOpened())
 	{
 		cerr << "Error: the ouput stream is not opened.\n";
-	}  
+	}
 
 	int frameNumber=0;
 	int key = 0;
@@ -113,86 +113,86 @@ int main (int argc, char * const argv[])
 	namedWindow("Output");
 
 	while(wasOk && key!=27)
-  	{
-    	frameNumber++;
-        
-    	//Read the next frame
-    	wasOk = input.read(inFrame2);
+	{
+		frameNumber++;
 
-    	//Check if the frame has been read successfully
-    	if (!wasOk)
-    
-    		break;
+		//Read the next frame
+		wasOk = input.read(inFrame2);
 
-    	//Subtract one frame to the previous one
-    	absdiff(inFrame2, inFrame, outFrame);
-    	
-    	//Generate a segmented mask
-    	Segmented_mask = outFrame > T;
+		//Check if the frame has been read successfully
+		if (!wasOk)
 
-    	//Create a structuring element of (2*radius+1)X(2*radius+1) 
-    	kernel = getStructuringElement(MORPH_RECT, Size(2*radius + 1, 2*radius + 1), Point(radius, radius));
-    	//Apply the opening operation to the segmented mask
-    	morphologyEx(Segmented_mask, closing, MORPH_CLOSE, kernel);
-    	//Apply the closing operation to the segmented mask
-    	morphologyEx(Segmented_mask, opening, MORPH_OPEN, kernel);
+			break;
 
-    	//opening + closing
-    	Segmented_mask = opening + closing;
+		//Subtract one frame to the previous one
+		absdiff(inFrame2, inFrame, outFrame);
+
+		//Generate a segmented mask
+		Segmented_mask = outFrame > T;
+
+		//Create a structuring element of (2*radius+1)X(2*radius+1) 
+		kernel = getStructuringElement(MORPH_RECT, Size(2*radius + 1, 2*radius + 1), Point(radius, radius));
+		//Apply the opening operation to the segmented mask
+		morphologyEx(Segmented_mask, closing, MORPH_CLOSE, kernel);
+		//Apply the closing operation to the segmented mask
+		morphologyEx(Segmented_mask, opening, MORPH_OPEN, kernel);
+
+		//opening + closing
+		Segmented_mask = opening + closing;
 		
-    	//Split the segmented mask in 3 channels
-    	split(Segmented_mask, splited);
-    	//Channel0 AND channel1
-    	bitwise_and(splited[0], splited[1], result);
-    	//(Channel0 AND channel1) AND channel2
-    	bitwise_and(result, splited[2], result);
+		//Split the segmented mask in 3 channels
+		split(Segmented_mask, splited);
+		//Channel0 AND channel1
+		bitwise_and(splited[0], splited[1], result);
+		//(Channel0 AND channel1) AND channel2
+		bitwise_and(result, splited[2], result);
 
-    	//Multiply by 255 to make the image brighter
-    	result *= 255;
+		//Multiply by 255 to make the image brighter
+		result *= 255;
 
-    	channels.push_back(result);
-    	channels.push_back(result);
-    	channels.push_back(result);
+		channels.push_back(result);
+		channels.push_back(result);
+		channels.push_back(result);
 
-    	//Adds 2 channels to the segmented mask to have a 3 channels segmented mask
-    	merge(channels, Segmented_mask3C);
+		//Adds 2 channels to the segmented mask to have a 3 channels segmented mask
+		merge(channels, Segmented_mask3C);
 
-    	//Apply the segmeted mask of 3 channels to get the detected object
-    	foreground = inFrame2&Segmented_mask3C;
+		//Apply the segmeted mask of 3 channels to get the detected object
+		foreground = inFrame2&Segmented_mask3C;
 
-    	//Write the segmented mask to the video
-    	output.write(Segmented_mask3C);
+		//Write the segmented mask to the video
+		output.write(Segmented_mask3C);
 
-    	//Show the detection of the foreground
-    	imshow ("Threshold", Segmented_mask3C);
-    	//Show the detected object
-    	imshow("Output", foreground);
-    	     
-    	//Read another frame
-    	wasOk=input.read(inFrame);
+		//Show the detection of the foreground
+		imshow ("Threshold", Segmented_mask3C);
+		//Show the detected object
+		imshow("Output", foreground);
+		     
+		//Read another frame
+		wasOk=input.read(inFrame);
 
-    	if (!wasOk)
+		if (!wasOk)
 
-    		break;
+			break;
 		
-    	key = waitKey(20);
+		key = waitKey(20);
 
-    	//If the spacebar is pressed, save the current frame
-    	if(key == 32){
+		//If the spacebar is pressed, save the current frame
+		if(key == 32){
 
-    		sprintf(out_image, "out_%d.jpg", frameNumber);
-    		imwrite(out_image, foreground);
-    	}
+			sprintf(out_image, "out_%d.jpg", frameNumber);
+			imwrite(out_image, foreground);
+		}
 
-    	//Clear the vector
-    	channels.clear();
-    }
+		//Clear the vector
+		channels.clear();
+	}
 
-    //Close the streams
-    input.release();
-    output.release();
+	//Close the streams
+	input.release();
+	output.release();
 
-    return 0;
+	return 0;
 }
 
 void on_trackbar(int, void *)
@@ -205,12 +205,11 @@ bool isNumber(const char *filein)
 {
 	for(int i = 0; i < strlen(filein); i++){
 
-	  	if(!isdigit(filein[i])){
+		if(!isdigit(filein[i])){
 
-	  		return false;
-	  	}
+			return false;
+		}
 	}
 
 	return true;
 }
-
